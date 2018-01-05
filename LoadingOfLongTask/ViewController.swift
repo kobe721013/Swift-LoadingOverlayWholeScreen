ff2e87723e234ea9e5fbe9422516035f9813056f
kobe Lin <kobebryant721013@gmail.com>
2018-01-05 23:00:32 +0800

    Initial Commit

---
+++ LoadingOfLongTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingOfLongTask
+//
+//  Created by kobe on 2018/1/5.
+//  Copyright © 2018年 kobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
